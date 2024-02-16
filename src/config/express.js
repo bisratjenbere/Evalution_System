c681ee14d95bee7e72a2291eff611156a7218a22
bisrat jenbere <jenberehenok@gmail.com>
2024-02-16 22:26:15 +0300

    express configeration is added

---
+++ src/config/express.js
@@ -0,0 +1,27 @@
+import express from "express";
+import * as environments from "../config/environments.js";
+import cloudinary from "cloudinary";
+import helmet from "helmet";
+import mongoSanitazer from "express-mongo-sanitize";
+import allRoutes from "./routes.js";
+
+import errorHandlerMiddleware from "../middleware/errorHandler.js";
+cloudinary.config({
+  cloud_name: environments.CLOUD_Name,
+  api_key: environments.CLOUD_KEY,
+  api_secret: environments.CLOUD_SECREAT,
+});
+import cors from "cors";
+const app = express();
+app.use(cors());
+app.use(mongoSanitazer());
+app.use(express.json());
+app.use(helmet());
+
+app.use("/api/v1", allRoutes);
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandlerMiddleware);
+export default app;
