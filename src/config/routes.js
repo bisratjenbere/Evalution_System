bad6b6ab8549bc3152d9ff422b7517752def6920
bisrat jenbere <jenberehenok@gmail.com>
2024-02-20 23:22:17 +0300

    resources are defined

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,20 @@
 import express from "express";
-import userRoutes from "../routes/userRoute.js";
+import userRouter from "../routes/userRoutes.js";
+import courseRouter from "../routes/courseRoute.js";
+import departmentRouter from "../routes/departmentRoute.js";
+import collegeRouter from "../routes/collegeRoutes.js";
+import complaintRouter from "../routes/complaintRoutes.js";
+import apprisalTempleteRouter from "../routes/appraisalTemplateRoutes.js";
+import appraisalCycleRouter from "../routes/appraisalCycleRoutes.js";
 
 const router = express.Router();
-console.log("in route");
-router.use("/user", userRoutes);
+
+router.use("/user", userRouter);
+router.use("/department", departmentRouter);
+router.use("/college", collegeRouter);
+router.use("/cycle", appraisalCycleRouter);
+router.use("/complaint", complaintRouter);
+router.use("/course", courseRouter);
+router.use("/templete", apprisalTempleteRouter);
 
 export default router;
