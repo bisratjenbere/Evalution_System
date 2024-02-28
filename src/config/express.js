fdc848d0050513f769575210dd891b63bf94f1d1
bisrat jenbere <jenberehenok@gmail.com>
2024-02-28 22:04:47 +0300

    express app is added

--- src/config/express.js
+++ src/config/express.js
@@ -4,6 +4,8 @@ import cloudinary from "cloudinary";
 import helmet from "helmet";
 import mongoSanitazer from "express-mongo-sanitize";
 import allRoutes from "./routes.js";
+import cookieParser from "cookie-parser";
+import AppError from "../utils/appError.js";
 
 import errorHandlerMiddleware from "../middleware/errorHandler.js";
 cloudinary.config({
@@ -18,6 +20,7 @@ app.use(mongoSanitazer());
 app.use(express.json());
 app.use(helmet());
 
+app.use(cookieParser(environments.JWT_KEY));
 app.use("/api/v1", allRoutes);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
