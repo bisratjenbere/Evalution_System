6be2935a7143a3523f612ad579733684753a1118
bisrat jenbere <jenberehenok@gmail.com>
2024-03-05 15:27:31 +0300

    endpoint is modfied

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,16 +5,16 @@ import departmentRouter from "../routes/departmentRoute.js";
 import collegeRouter from "../routes/collegeRoutes.js";
 import complaintRouter from "../routes/complaintRoutes.js";
 import apprisalTempleteRouter from "../routes/appraisalTemplateRoutes.js";
+import evaluationRouter from "../routes/reviewRoute.js";
 import appraisalCycleRouter from "../routes/appraisalCycleRoutes.js";
 import { authenticateUser } from "../middleware/authMiddleware.js";
 const router = express.Router();
-
 router.use("/users", userRouter);
 router.use("/departments", authenticateUser, departmentRouter);
 router.use("/colleges", authenticateUser, collegeRouter);
 router.use("/cycles", authenticateUser, appraisalCycleRouter);
+router.use("/reviews", authenticateUser, evaluationRouter);
 router.use("/complaints", authenticateUser, complaintRouter);
 router.use("/courses", authenticateUser, courseRouter);
 router.use("/templetes", authenticateUser, apprisalTempleteRouter);
-
 export default router;
