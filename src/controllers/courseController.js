48b7556d9460650c6a16ee45d3ddaf4b298d0078
bisrat jenbere <jenberehenok@gmail.com>
2024-03-18 16:53:15 +0300

    updated

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import Course from "../models/courseModel.js";
 import catchAsync from "../utils/catchAsync.js";
+import XLSX from "xlsx";
 import {
   deleteOne,
   updateOne,
@@ -51,3 +52,49 @@ export const getCoursesForDepartment = catchAsync(async (req, res, next) => {
     courses: activeCourses,
   });
 });
+
+async function processDataAndSave(data, req) {
+  try {
+    const headDeptID = req.user._id;
+
+    for (let row = 1; row < data.length; row++) {
+      const newCourse = new Course({
+        code: data[row][0],
+        name: data[row][1],
+        semester: parseInt(data[row][2]),
+        batch: parseInt(data[row][3]),
+        department: headDeptID,
+        startDate: new Date(data[row][4]),
+        endDate: new Date(data[row][5]),
+        instructor: data[row][6],
+      });
+
+      await newCourse.save();
+    }
+
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export const uploadCourse = async (req, res) => {
+  try {
+    console.log("iniguf");
+    const workbook = XLSX.read(req.file.buffer, { type: "buffer" });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    const success = await processDataAndSave(data, req);
+
+    if (success) {
+      return res.json({ status: "sucess", msg: "Courses successfully saved." });
+    } else {
+      return res.json({ status: "failed", msg: "Error processing data." });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({ status: "failed", msg: "Error uploading file." });
+  }
+};
