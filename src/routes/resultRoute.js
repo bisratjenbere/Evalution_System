4cee82c42fcddc9389b00ad13175bc38c79ad82f
bisrat jenbere <jenberehenok@gmail.com>
2024-03-18 16:53:55 +0300

    updated

--- src/routes/resultRoute.js
+++ src/routes/resultRoute.js
@@ -5,15 +5,18 @@ import {
   approveEvalutionResult,
   deleteEvalutionResult,
   getAllEvalutinResult,
+  getHeadEvalution,
+  getTeamLeaderEvalution,
+  getEvalutionByDepartmentID,
 } from "../controllers/resultController.js";
-
 const router = express.Router();
-
 router.get("/final-result", getFinalResult);
 router.get("/detailed-result", getDetailedResult);
 router.patch("/approve-result/:id", approveEvalutionResult);
 router.delete("/delete-result/:id", deleteEvalutionResult);
-router.get("/all-results", getAllEvalutinResult);
+router.get("/head-results", getHeadEvalution);
+router.get("/team-leader-results", getTeamLeaderEvalution);
+router.get("/result-by-department/:id", getEvalutionByDepartmentID);
 router.get("/", getAllEvalutinResult);
 
 export default router;
